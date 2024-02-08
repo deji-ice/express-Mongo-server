92c81265ab38b8cea6e2d7538cce68eaa674b018
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-08 09:34:40 +0100

    set up mongodb connection

---
+++ db/conn.js
@@ -0,0 +1,18 @@
+import mongodb from "mongodb";
+
+const { MongoClient } = mongodb;
+
+const connectionString = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(connectionString, { useUnifiedTopology: true });
+
+let conn;
+try {
+  conn = await client.connect();
+} catch (e) {
+  console.error(e);
+}
+
+let db = conn.db("sample_mflix");
+
+export default db;
