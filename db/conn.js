2f4353bdcf90db01a410dfcbde4f6da37aa1a80a
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-02-09 11:06:15 +0100

    setting up mongoose connection

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,39 @@
-import mongodb from "mongodb";
+import mongoose from 'mongoose';
 
-const { MongoClient } = mongodb;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
 
-const connectionString = process.env.ATLAS_URI || "";
+export default connectDB;
 
-const client = new MongoClient(connectionString, { useUnifiedTopology: true });
 
-let conn;
-try {
-  conn = await client.connect();
-} catch (e) {
-  console.error(e);
-}
 
-let db = conn.db("sample_mflix");
 
-export default db;
+// import mongodb from "mongodb";
+
+// const { MongoClient } = mongodb;
+
+// const connectionString = process.env.ATLAS_URI || "";
+
+// const client = new MongoClient(connectionString, { useUnifiedTopology: true });
+
+// let conn;
+// try {
+//   conn = await client.connect();
+// } catch (e) {
+//   console.error(e);
+// }
+
+// let db = conn.db("sample_mflix");
+
+// export default db;
